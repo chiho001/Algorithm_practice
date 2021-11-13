f2902cfdef1a522056dc36523e1e80d901309e66
chiho001@naver.com <you@your-domain.com>
2021-11-14 01:04:32 +0900

    data structure 조금 바꿔서 다시 정리

--- Greedy/11399_ATM.cpp
+++ Greedy/11399_ATM.cpp
@@ -1,28 +1,44 @@
+// 입력
+// 첫째 줄에 회의의 수 N(1 ≤ N ≤ 100,000)이 주어진다. 
+// 둘째 줄부터 N+1 줄까지 각 회의의 정보가 주어지는데 , 이것은 공백을 사이에 두고 회의의 시작시간과 끝나는 시간이 주어진다 
+// 시작 시간과 끝나는 시간은 2^31 - 1보다 작거나 같은 자연수 또는 0이다.
+
 #include<iostream>
-#include<vector>
-#include<stdio.h>
-#include <bits/stdc++.h>
+#include<bits/stdc++.h>
+
 using namespace std;
 
-
-int main() {
-    int N;
-    int P;
-    int output=0;
-    cin >> N;
-    vector<int> vec;
-
-    for(int i=0;i<N; i++)
+int main()
+{
+    int Num_meeting= 0; // 회의의 수 N(1 ≤ N ≤ 100,000)
+    int max_meeting_num = 1;
+    int end,begin;
+    cin >> Num_meeting;
+    vector<pair<int,int>> schedule;
+    
+    for(int i=0; i<Num_meeting; i++)
     {
-        cin >> P;
-        vec.emplace_back(P);
+        cin >> begin >> end;
+        schedule.push_back(make_pair(end,begin));
     }
-    sort(vec.begin(),vec.end());
+    
+    sort(schedule.begin(),schedule.end());
 
-    for(int i=0; i< N ; i++)
-    {
-        output += vec[i]*(N-i);
-    }
-    cout << output;
+    int time = schedule[0].first;
+	for (int i = 1 ;i < Num_meeting; i++) 
+	{
+		if (time <= schedule[i].second)
+		{
+			max_meeting_num++;
+			time = schedule[i].first;
+		}
+	}
+
+
+    cout << max_meeting_num;
+
+
+
     return 0;
 }
+
